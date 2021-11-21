7cd73b3b73eb994b2de69171e8ae10b81d65c3e6
Mocarafee <harif.rp05@gmail.com>
2021-11-21 10:55:12 +0700

    platina: Switch to prebuilt kernel
    * Yet this still need the kranul sauce for generated_kernel
    
    Change-Id: I39716bdf01804ff137cc45e1d7365da1df62ace2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,8 +95,8 @@ BOARD_KERNEL_CMDLINE := \
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
-TARGET_KERNEL_CONFIG := platina_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/platina
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_PREBUILT_KERNEL := device/xiaomi/platina-kernel/Image.gz-dtb
 
 # Media
 TARGET_USES_ION := true
